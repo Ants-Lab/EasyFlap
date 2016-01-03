4f975b83aa71198afc22a54ba31b403fc7453bc9
Nathan <nathsou.fr@gmail.com>
2016-01-03 06:21:24 +0100

    One must now log in to acces the server if not on the same network.

--- Interface/app.js
+++ Interface/app.js
@@ -1,84 +1,138 @@
 var express = require('express'),
 	app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    fs = require('fs'),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	fs = require('fs'),
 	pub_ip = require('public-ip'),
-	loc_ip = require('ip');
-  
-var cfg;
+	loc_ip = require('ip'),
+	crypto = require('crypto');
+
+var cfg, users;
 
 //Get server's public and local ip
-pub_ip.v4(function(err, pubIp){
-	
+pub_ip.v4(function (err, pubIp) {
+
 	var params = {};
-	
+
+	params.ip = {};
+
 	if (err) throw err;
-	
-	params.public = pubIp;
-	
-	params.local = loc_ip.address();
-	
+
+	params.ip.public = pubIp;
+
+	params.ip.local = loc_ip.address();
+
 	init(params);
-	
+
 });
 
 
-var init = function(params){
-	
+var init = function (params) {
+
 	console.log(params);
-	
+
 	//Read config.json
 	fs.readFile('./public/config.json', 'utf8', function (err, data) {
-		  if (err) throw err;
-		  cfg = JSON.parse(data);
+			if (err) throw err;
+			cfg = JSON.parse(data);
 
-		//Use ejs
-		app.set('view engine', 'ejs');
+			//Read access.json
+			fs.readFile('./access.json', 'utf8', function (err, data) {
+				if (err) throw err;
+				users = JSON.parse(data);
 
-		app.use(express.static(__dirname + '/public'));
+				if (!("admin" in Object.keys(users))) {
+					users.admin = "raspberry";
+				}
 
-		app.get('/', function (req, res) {
-			res.render('pages/index', { params : params });
-		});
+				//Use ejs
+				app.set('view engine', 'ejs');
 
-		var sendError = function(socket, errorMsg){
-			console.log('Error: ' + errorMsg);
-			socket.emit('err', errorMsg);
-		};
+				app.use(express.static(__dirname + '/public'));
 
-	  io.sockets.on('connection', function (socket) {
+				app.get('/', function (req, res) {
+					params.isLocalAccess = req.get('host') === params.ip.local + ':1621';
+					res.render('pages/index', {
+						params: params
+					});
+				});
 
-		//Send config data
+				var sendError = function (socket, errorMsg) {
+					console.log('Error: ' + errorMsg);
+					socket.emit('err', errorMsg);
+				};
 
-		socket.emit('config', cfg);
+				io.sockets.on('connection', function (socket) {
+					
+					socket.session = {};
+					socket.session.isLocalAccess = params.isLocalAccess;
 
-		socket.on('configUpdate', function(data){
-			var date = new Date();
-			console.log('Config file got updated: ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
+					//Send config data
 
-			console.log({ path: data.path, value: data.newVal });
+					socket.on('config_req', function(){
+						socket.emit('config', cfg);
+					});
 
-			(function(obj, value, path) {
-				path = path.split('.');
-				for (i = 0; i < path.length - 1; i++)
-					obj = obj[path[i]];
+					socket.on('configUpdate', function (data) {
+						var date = new Date();
+						console.log('Config file got updated: ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
 
-				obj[path[i]] = value;
-			})(cfg, data.newVal, data.path);
+						console.log({
+							path: data.path,
+							value: data.newVal
+						});
+
+						(function (obj, value, path) {
+							path = path.split('.');
+							for (i = 0; i < path.length - 1; i++)
+								obj = obj[path[i]];
+
+							obj[path[i]] = value;
+						})(cfg, data.newVal, data.path);
 
 
-			fs.writeFile('./public/config.json', JSON.stringify(cfg), function(err){
-				if(err) console.log(err);
+						fs.writeFile('./public/config.json', JSON.stringify(cfg), function (err) {
+							if (err) console.log(err);
+						});
+
+						socket.broadcast.emit('configUpdate', data);
+					});
+
+					socket.on('login_attempt', function (loginData) {
+						
+						var err = null,
+							token = null,
+							success = false;
+						
+						if (loginData === null) {
+							if(socket.session.isLocalAccess){
+								success = true;
+							}
+						} else if (loginData.user in users) {
+							if (users[loginData.user] === loginData.pass) {
+								success = true;
+							} else {
+								err = "Wrong password"; //Wrong password
+							}
+						} else {
+							err = "Wrong username"; //Wrong username
+						}
+
+						if (success) {
+							token = crypto.randomBytes(64).toString('hex');
+							socket.session.token = token;
+						}
+
+						socket.emit('login_response', { err: err, token: token });
+					});
+
+				});
+
+				server.listen(1621);
+
 			});
 
-			socket.broadcast.emit('configUpdate', data);
-		});
+		}
 
-	  });
-
-	  server.listen(1621);
-
-	});
-	
-}
+	)
+};
--- Interface/public/EasyFlapClient.js
+++ Interface/public/EasyFlapClient.js
@@ -1,22 +1,24 @@
 var cfg, copyCfg, updates = -1,
-	colors = ['#27ae60', '#2980b9', '#c0392b', '#8e44ad'];
+	colors = ['#27ae60', '#2980b9', '#c0392b', '#8e44ad'],
+	socket,
+	token;
 
-var checkPublicAccess = function(callback){
-	
-	if(location.host === params.public + ':1621'){
+var checkPublicAccess = function (callback) {
+
+	if (params.isLocalAccess) {
 		callback(true);
 		return;
 	}
-	
-	var sock = io.connect(params.public + ':1621');
-	sock.on('connect', function(){
+
+	var sock = io.connect(params.ip.public + ':1621');
+	sock.on('connect', function () {
 		sock.disconnect();
 		sock.close();
 		callback(true);
 		return;
 	});
-	
-	sock.on('connect_error', function(){
+
+	sock.on('connect_error', function () {
 		sock.disconnect();
 		sock.close();
 		callback(false);
@@ -24,211 +26,120 @@ var checkPublicAccess = function(callback){
 	});
 };
 
-if(location.host === params.local + ':1621'){
-	var socket = io.connect(params.local + ':1621');
-}else{ //Whatever the host is, try to connect to the public ip of the server
-	var socket = io.connect(params.public + ':1621');
-}
+var init = function () {
 
-socket.on('connect_error', function (message) {
-	console.log('Failed to connect to ' + params.public + ':1621' + ' trying to contact ' + params.local + ':1621' + '.');
-	socket.io.uri = params.local + ':1621';
-});
-
-socket.on('connect', function(){
-	console.log("Connected to : " + socket.io.uri);
-});
-
-socket.on('config', function (config) {
+	//Request config.json
+	socket.emit('config_req', null);
 	
-	updates++;
-
-	cfg = config;
-	copyCfg = (JSON.parse(JSON.stringify(cfg)));
-
-	$(window).bind('hashchange', navbarHandler);
-
-	$(window).trigger('hashchange');
-
-});
-
-socket.on('configUpdate', function (data) {
-
-	console.log(data);
-
-	copyCfg = (JSON.parse(JSON.stringify(cfg)));
-
-	updates++;
-
-	(function (obj, value, path) {
-		path = path.split('.');
-		for (i = 0; i < path.length - 1; i++)
-			obj = obj[path[i]];
-
-		obj[path[i]] = value;
-	})(cfg, data.newVal, data.path);
-
-
-	navbarHandler();
-
-});
-
-var emitConfigChange = function () {
-
-	var diffs = DeepDiff(copyCfg, cfg);
-
-	if (diffs) {
-
-		diffs.forEach(function (diff) {
-			if (diff.kind === 'E') { //Property edited
-				socket.emit('configUpdate', {
-					path: diff.path.join('.'),
-					newVal: diff.rhs 
-				});
-			}
-		});
-
-	}
-
-	copyCfg = (JSON.parse(JSON.stringify(cfg)));
-
-};
-
-var editShutterSettings = function (id) {
-
-	//Update the form
-	$('#shutterName').val(cfg.shutters[id - 1].loc);
-
-	var applyChangesHandler = function () {
-		var loc = $('#shutterName').val();
-		$('#configBoxTitle').text('Configure ' + loc.toLowerCase() + ' shutters');
-		cfg.shutters[id - 1].loc = loc;
-		$('#table-loc-' + id).text(loc);
-		emitConfigChange();
-	};
-
-	$('#configBoxTitle').text('Configure ' + cfg.shutters[id - 1].loc.toLowerCase() + ' shutters');
-
-	$('#applyChanges').bind('click', applyChangesHandler);
-
-	$('#configBox').modal();
-
-	$('#configBox').on('hide.bs.modal', function () {
-		$('#applyChanges').unbind('click', applyChangesHandler);
+	socket.on('connect_error', function (message) {
+		console.log('Failed to connect to ' + params.ip.public + ':1621' + ' trying to contact ' + params.ip.local + ':1621' + '.');
+		socket.io.uri = params.ip.local + ':1621';
 	});
-};
 
-var navbarHandler = function () {
+	socket.on('config', function (config) {
 
-	$('#container').empty();
+		updates++;
 
-	var page = location.hash.substr(1);
-	page = page !== '' ? page : 'Control-Panel';
+		cfg = config;
+		copyCfg = (JSON.parse(JSON.stringify(cfg)));
 
-	switch (page) {
+		$(window).bind('hashchange', navbarHandler);
 
-		case "Control-Panel":
+		$(window).trigger('hashchange');
 
-			$('#container').html("<table class='table table-bordered table-hover' id='table'></table>");
+	});
 
-			$('#table').html(
-				'<tr><th>id</th><th>Location</th><th>Opening angle</th><th>Configure</th></tr>'
-			);
+	socket.on('configUpdate', function (data) {
 
-			cfg.shutters.forEach(function (shutter) {
+		console.log(data);
 
-				$('#table').append('<tr id="tr-' + shutter.id + '">' + '<td>' + shutter.id + '</td>' + '<td id="table-loc-' + shutter.id + '">' + shutter.loc + '</td>' + '<td><div id="slider-' + shutter.id + '" class="slider"></div></td>' + '<td><button type="button" onclick="editShutterSettings(' + shutter.id + ');"' + 'class="btn btn-default" aria-label="Configure">' + '<span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></button>' + '</td></tr>');
+		copyCfg = (JSON.parse(JSON.stringify(cfg)));
 
-				$('#slider-' + shutter.id).roundSlider({
-					radius: 60,
-					width: 14,
-					circleShape: 'half-top',
-					handleSize: "24,12",
-					handleShape: "square",
-					sliderType: "min-range",
-					showTooltip: 'false',
-					min: 0,
-					max: 180,
+		updates++;
 
-					change: function (e) {
-						shutter.targetAngle = e.value;
-						emitConfigChange();
-					}
-				});
+		(function (obj, value, path) {
+			path = path.split('.');
+			for (i = 0; i < path.length - 1; i++)
+				obj = obj[path[i]];
 
-				$('#slider-' + shutter.id + ' .rs-range-color').css('background-color', colors[(shutter.id - 1) % colors.length]);
+			obj[path[i]] = value;
+		})(cfg, data.newVal, data.path);
 
-				$('#slider-' + shutter.id).roundSlider('option', 'animation', false);
-				$('#slider-' + shutter.id).roundSlider('option', 'value', updates > 0 ? copyCfg.shutters[shutter.id - 1].targetAngle : 0);
-				$('#slider-' + shutter.id).roundSlider('option', 'animation', true);
-				$('#slider-' + shutter.id).roundSlider('option', 'value', shutter.targetAngle);
 
-				$('#tr-' + shutter.id).hover(
+		navbarHandler();
 
-					function () { //in
-						$('#slider-' + shutter.id + ' .rs-inner').css('background-color', 'whitesmoke');
-					},
+	});
 
-					function () { //out
-						$('#slider-' + shutter.id + ' .rs-inner').css('background-color', 'white');
-					}
+	var emitConfigChange = function () {
 
-				);
+		var diffs = DeepDiff(copyCfg, cfg);
 
+		if (diffs) {
+
+			diffs.forEach(function (diff) {
+				if (diff.kind === 'E') { //Property edited
+					socket.emit('configUpdate', {
+						path: diff.path.join('.'),
+						newVal: diff.rhs 
+					});
+				}
 			});
 
-			break;
+		}
 
-		case "Captors":
+		copyCfg = (JSON.parse(JSON.stringify(cfg)));
 
-			cfg.shutters.forEach(function (shutter) {
+	};
 
-				var attributes = [];
+	var editShutterSettings = function (id) {
 
-				$('#container').append('<h3>' + shutter.loc + '</h3>');
-				$('#container').append(
-					"<table class='table table-bordered table-hover' id='table-shutter-" + shutter.id + "'></table>"
+		//Update the form
+		$('#shutterName').val(cfg.shutters[id - 1].loc);
+
+		var applyChangesHandler = function () {
+			var loc = $('#shutterName').val();
+			$('#configBoxTitle').text('Configure ' + loc.toLowerCase() + ' shutters');
+			cfg.shutters[id - 1].loc = loc;
+			$('#table-loc-' + id).text(loc);
+			emitConfigChange();
+		};
+
+		$('#configBoxTitle').text('Configure ' + cfg.shutters[id - 1].loc.toLowerCase() + ' shutters');
+
+		$('#applyChanges').bind('click', applyChangesHandler);
+
+		$('#configBox').modal();
+
+		$('#configBox').on('hide.bs.modal', function () {
+			$('#applyChanges').unbind('click', applyChangesHandler);
+		});
+	};
+
+	var navbarHandler = function () {
+
+		$('#container').empty();
+
+		var page = location.hash.substr(1);
+		page = page !== '' ? page : 'Control-Panel';
+
+		switch (page) {
+
+			case "Control-Panel":
+
+				$('#container').html("<table class='table table-bordered table-hover' id='table'></table>");
+
+				$('#table').html(
+					'<tr><th>id</th><th>Location</th><th>Opening angle</th><th>Configure</th></tr>'
 				);
 
-				shutter.captors.forEach(function (captor) {
-					Object.keys(captor).forEach(function (attrib) {
-						if (attributes.indexOf(attrib) === -1) {
-							attributes.push(attrib);
-						}
+				cfg.shutters.forEach(function (shutter) {
+
+					$('#table').append('<tr id="tr-' + shutter.id + '">' + '<td>' + shutter.id + '</td>' + '<td id="table-loc-' + shutter.id + '">' + shutter.loc + '</td>' + '<td><div id="slider-' + shutter.id + '" class="slider"></div></td>' + '<td><button id="openConfig-' + shutter.id + '" type="button" class="btn btn-default" aria-label="Configure">' + '<span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></button>' + '</td></tr>');
+
+					$('#openConfig-' + shutter.id).on('click', function(){
+						editShutterSettings(shutter.id);
 					});
-				});
-
-				attributes.forEach(function (attrib) {
-
-					var vals = '';
-
-					shutter.captors.forEach(function (cpt) {
-
-						var format;
-
-						if (attrib === 'value' && cpt.type === 'position') {
-							format = '<div id="slider-' + shutter.id + '" class="slider"></div>';
-						} else {
-							format = cpt[attrib].length > 1 ? cpt[attrib][0].toUpperCase() + cpt[attrib].substr(1) : cpt[attrib];
-						}
-
-						vals += '<td>' + format + '</td>';
-
-					});
-
-					$('#table-shutter-' + shutter.id).append(
-						'<tr ' + (attrib === 'value' ? 'id="tr-' + shutter.id + '"' : '') + '><th>' + attrib[0].toUpperCase() + attrib.substr(1) + '</th>' + vals + '</tr>'
-					);
-
-					var value;
-
-					for (var i = 0; i < shutter.captors.length; i++) {
-						if (shutter.captors[i].type === 'position') {
-							value = Number.parseInt(shutter.captors[i].value);
-							break;
-						}
-					}
-
+					
 					$('#slider-' + shutter.id).roundSlider({
 						radius: 60,
 						width: 14,
@@ -239,12 +150,19 @@ var navbarHandler = function () {
 						showTooltip: 'false',
 						min: 0,
 						max: 180,
-						animation: false,
-						value: value,
-						disabled: true
+
+						change: function (e) {
+							shutter.targetAngle = e.value;
+							emitConfigChange();
+						}
 					});
 
 					$('#slider-' + shutter.id + ' .rs-range-color').css('background-color', colors[(shutter.id - 1) % colors.length]);
+
+					$('#slider-' + shutter.id).roundSlider('option', 'animation', false);
+					$('#slider-' + shutter.id).roundSlider('option', 'value', updates > 0 ? copyCfg.shutters[shutter.id - 1].targetAngle : 0);
+					$('#slider-' + shutter.id).roundSlider('option', 'animation', true);
+					$('#slider-' + shutter.id).roundSlider('option', 'value', shutter.targetAngle);
 
 					$('#tr-' + shutter.id).hover(
 
@@ -260,13 +178,138 @@ var navbarHandler = function () {
 
 				});
 
-			});
+				break;
 
-			break;
+			case "Captors":
 
-		case "Program":
+				cfg.shutters.forEach(function (shutter) {
 
-			break;
+					var attributes = [];
+
+					$('#container').append('<h3>' + shutter.loc + '</h3>');
+					$('#container').append(
+						"<table class='table table-bordered table-hover' id='table-shutter-" + shutter.id + "'></table>"
+					);
+
+					shutter.captors.forEach(function (captor) {
+						Object.keys(captor).forEach(function (attrib) {
+							if (attributes.indexOf(attrib) === -1) {
+								attributes.push(attrib);
+							}
+						});
+					});
+
+					attributes.forEach(function (attrib) {
+
+						var vals = '';
+
+						shutter.captors.forEach(function (cpt) {
+
+							var format;
+
+							if (attrib === 'value' && cpt.type === 'position') {
+								format = '<div id="slider-' + shutter.id + '" class="slider"></div>';
+							} else {
+								format = cpt[attrib].length > 1 ? cpt[attrib][0].toUpperCase() + cpt[attrib].substr(1) : cpt[attrib];
+							}
+
+							vals += '<td>' + format + '</td>';
+
+						});
+
+						$('#table-shutter-' + shutter.id).append(
+							'<tr ' + (attrib === 'value' ? 'id="tr-' + shutter.id + '"' : '') + '><th>' + attrib[0].toUpperCase() + attrib.substr(1) + '</th>' + vals + '</tr>'
+						);
+
+						var value;
+
+						for (var i = 0; i < shutter.captors.length; i++) {
+							if (shutter.captors[i].type === 'position') {
+								value = Number.parseInt(shutter.captors[i].value);
+								break;
+							}
+						}
+
+						$('#slider-' + shutter.id).roundSlider({
+							radius: 60,
+							width: 14,
+							circleShape: 'half-top',
+							handleSize: "24,12",
+							handleShape: "square",
+							sliderType: "min-range",
+							showTooltip: 'false',
+							min: 0,
+							max: 180,
+							animation: false,
+							value: value,
+							disabled: true
+						});
+
+						$('#slider-' + shutter.id + ' .rs-range-color').css('background-color', colors[(shutter.id - 1) % colors.length]);
+
+						$('#tr-' + shutter.id).hover(
+
+							function () { //in
+								$('#slider-' + shutter.id + ' .rs-inner').css('background-color', 'whitesmoke');
+							},
+
+							function () { //out
+								$('#slider-' + shutter.id + ' .rs-inner').css('background-color', 'white');
+							}
+
+						);
+
+					});
+
+				});
+
+				break;
+
+			case "Program":
+
+				break;
+		}
+
+	};
+};
+
+window.onload = function () {
+	if (params.isLocalAccess) {
+		socket = io.connect(params.ip.local + ':1621');
+	} else { //Whatever the host is, try to connect to the public ip of the server
+		socket = io.connect(params.ip.public + ':1621');
 	}
+
+	socket.on('connect', function () {
+		console.log("Connected to : " + socket.io.uri);
+
+		if (params.isLocalAccess) {
+			socket.emit('login_attempt', null);
+		} else {
+			$('#loginButton').on('click', function () {
+				socket.emit('login_attempt', { "user": $('#user').val(), "pass": $('#pass').val() });
+			});
+			
+			$(window).on('keypress', function(e){
+				if(e.keyCode === 13){
+					$('#loginButton').trigger('click');
+				}
+			});
+		}
+	});
+	
+	socket.on('login_response', function(res){
+		
+		console.log(res);
+		
+		if(res.err === null && res.token !== null){
+			token = res.token;
+			$('#loginBox').empty();
+			$('#navbar').show();
+			init();
+		}else{
+			alertify.error(res.err);
+		}
+	});
 
 };
